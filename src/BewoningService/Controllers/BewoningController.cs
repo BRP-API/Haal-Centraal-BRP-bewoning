01ffad6c8022add55d90409f6590c3bc0d45f6ae
Melvin Lee <melvin.lee@iswish.nl>
2023-06-26 16:11:59 +0200

    chore(mock): setup bewoningen endpoint

---
+++ src/BewoningService/Controllers/BewoningController.cs
@@ -0,0 +1,29 @@
+﻿using HaalCentraal.BewoningService.Generated;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaalCentraal.BewoningService.Controllers;
+
+[ApiController]
+public class BewoningController : Generated.ControllerBase
+{
+    private readonly ILogger<BewoningController> _logger;
+
+    public BewoningController(ILogger<BewoningController> logger)
+    {
+        _logger = logger;
+    }
+
+    public override async Task<ActionResult<GbaBewoningenQueryResponse>> Bewoningen([FromBody] BewoningenQuery body)
+    {
+        _logger.LogDebug("Request. {headers} {@body}", HttpContext.Request.Headers, body);
+
+        var retval = body switch
+        {
+            _ => new GbaBewoningenQueryResponse()
+        };
+
+        _logger.LogDebug("Response. {@body}", retval);
+        
+        return Ok(retval);
+    }
+}
