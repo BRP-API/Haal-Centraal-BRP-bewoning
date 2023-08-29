01ad8045f1e0efe25e8c00dc4f9930e93db40e50
Melvin Lee <melvin.lee@iswish.nl>
2023-08-29 08:47:48 +0200

    feat(proxy): mTLS ondersteuning geïmplementeerd

--- src/BewoningProxy/Program.cs
+++ src/BewoningProxy/Program.cs
@@ -1,9 +1,14 @@
+using Bewoning.Infrastructure.mTls;
+using Bewoning.Infrastructure.Serilog;
 using BewoningProxy.Middlewares;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.ClearProviders();
+builder.Host.UseSerilog(SerilogHelpers.Configure());
 
 builder.Configuration.AddJsonFile(Path.Combine("configuration", "ocelot.json"))
                      .AddJsonFile(Path.Combine("configuration", $"ocelot.{builder.Environment.EnvironmentName}.json"), true)
@@ -11,7 +16,8 @@ builder.Configuration.AddJsonFile(Path.Combine("configuration", "ocelot.json"))
 
 // Add services to the container.
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddOcelot();
+builder.Services.AddOcelot()
+                .AddDelegatingHandler<X509Handler>(global: true);
 
 var app = builder.Build();
 
