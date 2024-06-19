6ca9755baaa53291e3deec5bc537a6bc58f56e89
Melvin Lee <melvin.lee@iswish.nl>
2024-06-19 14:31:24 +0200

    chore(proxy): aanduiding functie adres in onderzoek mapping toegevoegd

--- src/BewoningProxy/Profiles/BewonerProfile.cs
+++ src/BewoningProxy/Profiles/BewonerProfile.cs
@@ -26,11 +26,13 @@ public static class VerblijfplaatsInOnderzoekConverter
         {
             "080000" or
             "081000" or
+            "081010" or
             "081030" or
             "081100" or
             "081180" or
             "580000" or
             "581000" or
+            "581010" or
             "581030" or
             "581100" or
             "581180" => true,
